c38721251d44b5b63e1feca197861f6a0b384b93
adalll <adalll@mail.ru>
2019-07-13 23:04:44 +0700

    Refactoring Seasons to Hooks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import useLocation from './useLocation';
 
-class App extends React.Component {
+const App = () => {
+
+    const [lat, errorMessage] = useLocation();
+
+    let content;
+    if (errorMessage) {
+       content = <div>Error:{errorMessage}</div>;
+    } else if (lat) {
+        content = <SeasonDisplay lat={lat}/>;
+    } else {
+        content = <Spinner message="Please allow to get your location!" />;
+    }
+
+    return <div className="border red">{content}</div>
+};
+
+
+/*class App extends React.Component {
 
     state = {
         lat : null,
@@ -38,6 +56,6 @@ class App extends React.Component {
           </div>
         );
     }
-}
+}*/
 
 ReactDOM.render(<App />, document.querySelector("#root"));
